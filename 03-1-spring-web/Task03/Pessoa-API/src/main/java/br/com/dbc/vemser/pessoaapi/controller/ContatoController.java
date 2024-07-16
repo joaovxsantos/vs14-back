6519226d684f25510b9c7a2b5f79b460691ef4e5
João Vitor <joaovitor55670@gmail.com>
2024-07-16 11:33:53 -0300

    Task03

---
+++ 03-1-spring-web/Task03/Pessoa-API/src/main/java/br/com/dbc/vemser/pessoaapi/controller/ContatoController.java
@@ -0,0 +1,45 @@
+package br.com.dbc.vemser.pessoaapi.controller;
+
+
+import br.com.dbc.vemser.pessoaapi.entity.Contato;
+import br.com.dbc.vemser.pessoaapi.service.ContatoService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/contato") // localhost:8080/contato
+public class ContatoController {
+
+    private ContatoService contatoService;
+
+    public ContatoController() {
+        contatoService = new ContatoService();
+    }
+
+
+
+    @GetMapping
+    public List<Contato> list() {
+        return contatoService.list();
+    }
+
+    @GetMapping("/{idPessoa}")
+    public List<Contato> listByPessoa(@PathVariable("idPessoa") Integer idPessoa) {
+        return contatoService.listByIdPessoa(idPessoa);
+    }
+
+    @PostMapping
+    public Contato create(@RequestBody Contato contato) {
+        return contatoService.create(contato);
+    }
+
+    @PutMapping("/{idContato}")
+    public Contato update(@PathVariable("idContato") Integer idContato, @RequestBody Contato contatoAtualizar) throws Exception {
+        return contatoService.update(idContato, contatoAtualizar);
+    }
+
+    @DeleteMapping("/{idContato}")
+    public void delete(@PathVariable("idContato") Integer idContato) throws Exception {
+        contatoService.delete(idContato);
+    }
+}
---
+++ 03-1-spring-web/Task03/Pessoa-API/src/main/java/br/com/dbc/vemser/pessoaapi/service/ContatoService.java
@@ -0,0 +1,57 @@
+package br.com.dbc.vemser.pessoaapi.service;
+
+import br.com.dbc.vemser.pessoaapi.entity.Contato;
+import br.com.dbc.vemser.pessoaapi.repository.ContatoRepository;
+
+import java.util.List;
+
+public class ContatoService {
+
+    private ContatoRepository contatoRepository;
+
+    public ContatoService(){
+        contatoRepository = new ContatoRepository();
+    }
+
+    public Contato create(Contato contato){
+
+        // Validações
+
+        return contatoRepository.create(contato);
+    }
+
+    public List<Contato> list(){
+        return contatoRepository.list();
+    }
+
+    public Contato update(Integer id,
+                          Contato contatoAtualizar) throws Exception {
+        Contato contatoRecuperado = getContato(id);
+
+        contatoRecuperado.setIdContato(contatoAtualizar.getIdContato());
+        contatoRecuperado.setTipoContato(contatoAtualizar.getTipoContato());
+        contatoRecuperado.setNumero(contatoAtualizar.getNumero());
+        contatoRecuperado.setDescricao(contatoAtualizar.getDescricao());
+
+        return contatoRecuperado;
+    }
+
+    public void delete(Integer id) throws Exception {
+        Contato contatoRecuperado = getContato(id);
+        contatoRepository.delete(contatoRecuperado);
+    }
+
+    public List<Contato> listByIdPessoa(Integer idPessoa) {
+        return contatoRepository.listByIdPessoa(String.valueOf(idPessoa));
+    }
+
+    private Contato getContato(Integer id) throws Exception {
+        Contato contatoRecuperado = contatoRepository.list().stream()
+                .filter(contato -> contato.getIdContato().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new Exception("Contato não encontrado!"));
+        return contatoRecuperado;
+    }
+
+
+}
